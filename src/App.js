4072496032bd68c9a0ecd34660f56e9162a3e948
jovanovskmarko <marko.jovanovski66@gmail.com>
2023-09-17 12:20:07 +0200

    add userList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AddUser from "./Components/User/AddUser";
+import UsersList from "./Components/User/UsersList";
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList users={[]} />
     </div>
   );
 }
--- src/Components/User/AddUser.js
+++ src/Components/User/AddUser.js
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 import classes from "./AddUser.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
+  const [enteredUserName, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (e) => {
     e.preventDefault();
+    if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+
+    if (+enteredAge < 1) {
+      return;
+    }
+    console.log("nesto");
+    setEnteredAge("");
+    setEnteredUsername("");
   };
+
+  const usernameChangeHandler = (e) => {
+    setEnteredUsername(e.target.value);
+  };
+
+  const ageChangeHandler = (e) => {
+    setEnteredAge(e.target.value);
+  };
+
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
-        <input id='username' type='text' />
+        <input
+          id='username'
+          type='text'
+          onChange={usernameChangeHandler}
+          value={enteredUserName}
+        />
         <label htmlFor='age'>Age</label>
-        <input type='text' id='age' />
+        <input
+          type='text'
+          id='age'
+          onChange={ageChangeHandler}
+          value={enteredAge}
+        />
         <Button type='submit'>Add User</Button>
       </form>
     </Card>
